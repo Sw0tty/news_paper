a20fbd47f1c43d92749d97d2f7333c75ea669c8d
Swotty <swotty@yandex.ru>
2023-07-26 11:48:21 +0300

    Mod settings

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
+# Loading .env args
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a81g_qjd$!75o&vdzm1(dukm$04+%=ae_36%tnwt8h&!a8g)7)'
+SECRET_KEY = os.getenv('DJANGO_SECURITY_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
